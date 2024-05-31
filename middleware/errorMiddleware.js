c10756ebc200de9ca0322d24d4958d628bf2ce93
YongJR <kwyong2020@gmail.com>
2024-05-31 16:28:40 +0800

    added .env

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,11 +1,10 @@
 const errorMiddleware = (err, req, res, next) => {
-  console.log("This is an error middleware")
-  const statusCode = res.statusCode ? res.statusCode : 500
-  res.status(statusCode)
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "development" ? err.stack : null
-  })
-}
+  });
+};
 
-module.exports = errorMiddleware
+module.exports = errorMiddleware;
